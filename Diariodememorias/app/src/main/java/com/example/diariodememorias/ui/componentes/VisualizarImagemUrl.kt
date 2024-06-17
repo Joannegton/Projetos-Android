82af6e204af6369df5d0799251c4d84ecdab51b8
joannegton <wtg131998@gmail.com>
2024-06-17 17:31:34 -0300

    update

--- Diariodememorias/app/src/main/java/com/example/diariodememorias/ui/componentes/VisualizarImagemUrl.kt
+++ Diariodememorias/app/src/main/java/com/example/diariodememorias/ui/componentes/VisualizarImagemUrl.kt
@@ -3,6 +3,8 @@ package com.example.diariodememorias.ui.componentes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,13 +13,18 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.VectorProperty
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
-import coil.annotation.ExperimentalCoilApi
 import coil.compose.ImagePainter
-import coil.compose.rememberImagePainter
 
 @Composable
 fun VisualizadorImagemUrl(
@@ -29,13 +36,38 @@ fun VisualizadorImagemUrl(
         Box(modifier = Modifier
             .fillMaxSize()
             .background(Color(0x99000000))
-            .clickable { onDialogDismiss() }
         ) {
+
+            var scale = remember { mutableStateOf(1f) }
+            var offsetX = remember { mutableStateOf(0f) }
+            var offsetY = remember { mutableStateOf(0f) }
+            var transformOrigin = remember { mutableStateOf(TransformOrigin(0.5f, 0.5f)) }
 
             Image(
                 painter = imagePainter,
                 contentDescription = null,
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
+                    .graphicsLayer( //modificador de escala
+                        scaleX = scale.value,
+                        scaleY = scale.value,
+                        translationX = offsetX.value,
+                        translationY = offsetY.value,
+                        transformOrigin = transformOrigin.value
+                    )
+                    .pointerInput(Unit){//permite lidar com eventos de entrada do ponteiro, como toques na tela.
+                        detectTransformGestures { centroid, _, zoom, pan -> //permite detectar gestos de transformação, como pinça para zoom.
+                            scale.value *= zoom
+                            offsetX.value += pan
+                            offsetY.value += pan
+                            transformOrigin.value = TransformOrigin(centroid.x / size.width, centroid.y / size.height)
+                        }
+                        detectDragGestures { change, dragAmount ->
+                            change.consume()
+                            offsetX.value += dragAmount.x
+                        }
+                    }
+
             )
 
             Icon(
@@ -46,6 +78,7 @@ fun VisualizadorImagemUrl(
                     .size(50.dp)
                     .padding(top = 10.dp, end = 10.dp)
                     .align(Alignment.TopEnd)
+                    .clickable { onDialogDismiss() }
             )
         }
     }
