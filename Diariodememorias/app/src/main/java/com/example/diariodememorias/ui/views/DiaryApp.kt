e6432c8cce7cae14f9d1f7f508ec5189739771e3
joannegton <wtg131998@gmail.com>
2024-07-15 22:39:27 -0300

    update

--- Diariodememorias/app/src/main/java/com/example/diariodememorias/ui/views/DiaryApp.kt
+++ Diariodememorias/app/src/main/java/com/example/diariodememorias/ui/views/DiaryApp.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -24,7 +25,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
@@ -46,7 +46,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
@@ -154,7 +153,7 @@ fun CardMemoria(
         ) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Absolute.SpaceBetween,
+                horizontalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Text(
@@ -166,7 +165,8 @@ fun CardMemoria(
                     Icon(
                         imageVector = Icons.Default.Delete,
                         contentDescription = "Apagar memoria",
-                        tint = Color.Red
+                        tint = Color.Red,
+                        modifier = Modifier.size(30.dp)
                     )
                 }
             }
